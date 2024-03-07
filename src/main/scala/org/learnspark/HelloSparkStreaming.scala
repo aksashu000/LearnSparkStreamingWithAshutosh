1f8f2bd77a57f512d4f6de475c85b719aebbfe4f
Ashutosh Kumar <ashutokumar@paypal.com>
2024-03-07 18:01:55 +0530

    First commit

---
+++ src/main/scala/org/learnspark/HelloSparkStreaming.scala
@@ -0,0 +1,27 @@
+package org.learnspark
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.streaming.OutputMode
+import org.apache.spark.sql.types.{StringType, StructField, StructType, TimestampType}
+
+
+object HelloSparkStreaming{
+
+  def main(args: Array[String]): Unit = {
+    val sparkSession = SparkSession.builder().appName("Hello Spark Streaming").master("local[*]").getOrCreate()
+    val schema = StructType(List(
+      StructField("id", StringType, nullable = false),
+      StructField("eventTime", TimestampType, nullable = false),
+      StructField("clickedResource", StringType, nullable = false)
+    ))
+    val df = sparkSession.readStream.schema(schema).json("src/main/resources/data")
+    df.printSchema()
+
+    df
+      .writeStream
+      .format("console")
+      .start()
+      .awaitTermination()
+  }
+
+}
---
+++ src/main/scala/org/learnspark/streamingsources/SocketSourceWithCompleteOutputMode.scala
@@ -0,0 +1,39 @@
+package org.learnspark.streamingsources
+
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.streaming.{OutputMode, StreamingQuery}
+
+/**
+ * This is the example for socket source with complete output mode
+ * @author Ashutosh kumar
+ */
+object SocketSourceWithCompleteOutputMode {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder().appName("Hello Spark Streaming").master("local[*]").getOrCreate()
+
+    val df: DataFrame = spark
+      .readStream
+      .format("socket")
+      .options(Map("host" -> "localhost", "port" -> "8888"))
+      .load()
+
+    df.printSchema()
+
+    import spark.implicits._
+
+    val ds: Dataset[String] = df.as[String]
+
+    val wordCountDf: DataFrame = ds
+        .flatMap(_.split(" "))
+        .groupBy("value")
+        .count()
+
+    val query: StreamingQuery = wordCountDf
+      .writeStream
+      .outputMode(OutputMode.Complete())
+      .format("console")
+      .start()
+
+    query.awaitTermination()
+  }
+}
